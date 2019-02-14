7b4549b57e6a7211c0bc03b4d60ffb17b2904dfd
alessandramitie <alessandra.mitie@gmail.com>
2019-02-14 01:14:01 -0200

    complementacao de if else no script

--- cinema/script.js
+++ cinema/script.js
@@ -1,14 +1,48 @@
-let nome = prompt("Qual seu nome?");
-let idade = prompt("Qual sua idade?");
-let filme = prompt("Qual filme voce quer assistir?");
-
+let idade = prompt("Olá! Qual sua idade?");
+let filme = prompt("Digite o nome do filme que deseja assistir: \n Filme 1 \n Filme 2 \n Filme 3 \n Filme 4 \n Filme 5");
+let outro;
 //array a ser preenchido com os objetos
 filmes = [];
 
 //argumentos do objeto
-filme1 = {nome: "filme 1", classificacao: 14};
-filme2 = {nome: "filme 2", classificacao: 16};
-filme3 = {nome: "filme 3", classificacao: 18};
-filme4 = {nome: "filme 4", classificacao: 14};
-filme5 = {nome: "filme 5", classificacao: 16};
+filme1 = {nome: "Filme 1", classificacao: 14};
+filme2 = {nome: "Filme 2", classificacao: 16};
+filme3 = {nome: "Filme 3", classificacao: 18};
+filme4 = {nome: "Filme 4", classificacao: 14};
+filme5 = {nome: "Filme 5", classificacao: 16};
 
+if (filme == "Filme 1" || filme == "Filme 4"){
+    if (idade < 14){
+        alert(`Você não tem idade suficiente para assistir a este filme. Classificação: 14 anos`);
+        let outro = prompt("Deseja ver outro filme?");
+        if (outro == "sim"){
+            let filme = prompt("Digite o nome do filme que deseja assistir: \n Filme 1 \n Filme 2 \n Filme 3 \n Filme 4 \n Filme 5");
+        } else{
+            close();
+        }
+    } else{
+        alert(`Deseja comprar seu ingresso agora?`);
+    }
+} else if (filme == "Filme 2" || filme == "Filme 5"){
+    if (idade < 16){
+        alert(`Você não tem idade suficiente para assistir a este filme. Classificação: 16 anos.`);
+        let outro = prompt("Deseja ver outro filme?");
+        if (outro == "sim"){
+            let filme = prompt("Digite o nome do filme que deseja assistir: \n Filme 1 \n Filme 2 \n Filme 3 \n Filme 4 \n Filme 5");
+        } else{
+            close();
+        }
+    } else{
+        alert(`Deseja comprar seu ingresso agora?`);
+    }
+} else if (idade <18){
+    alert(`Você não tem idade suficiente para assistir a este filme. Classificação 18 anos`);
+    let outro = prompt("Deseja ver outro filme?");
+    if (outro == "sim"){
+        let filme = prompt("Digite o nome do filme que deseja assistir: \n Filme 1 \n Filme 2 \n Filme 3 \n Filme 4 \n Filme 5")
+    } else{
+        close();
+    }
+} else {
+    alert(`Deseja comprar seu ingresso agora?`);
+}
